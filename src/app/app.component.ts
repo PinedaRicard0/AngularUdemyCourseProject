46d4cfdb47084575c04244c4e31a28cf3daf5e99
Ricardo Pineda R <pinedaricardo07@gmail.com>
2019-05-18 13:00:47 -0500

    Adding navigation between option selected in header a data displayed in app componet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'CourseProject';
+  tabSelected:string = 'recipie'
+
+  onHeaderTabSelected(tab:string){
+    this.tabSelected =  tab;
+  }
 }
