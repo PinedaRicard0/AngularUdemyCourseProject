968aa9d924e377da2932dafec303c4d3accec104
Ricardo Pineda R <pinedaricardo07@gmail.com>
2019-05-19 14:41:31 -0500

    Allowing application to add new shopping list through shopping edit component to shopping list component

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Output, EventEmitter } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.module';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingEditComponent implements OnInit {
 
+  @ViewChild('amountInput') amountInput: ElementRef;
+  @Output('seIngredientAdded') ingredientAdded = new EventEmitter<Ingredient>();
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onAddClicked(nameInput:HTMLInputElement){
+    const nInput: string = nameInput.value;
+    const aInput: string = this.amountInput.nativeElement.value;
+    if(nInput !== '' && aInput !== '' && Number(aInput) > 0)
+    {
+      this.ingredientAdded.emit(
+        new Ingredient(nameInput.value,Number(this.amountInput.nativeElement.value))
+        )
+    }
   }
 
 }
